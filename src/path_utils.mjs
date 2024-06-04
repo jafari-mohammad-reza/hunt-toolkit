522ced6ff1d4753962767d23f7da1298b3b11ada
Mohammadreza Jafari <mohammadrezajafari.dev@gmail.com>
2024-06-04 12:04:06 +0330

    add pre installer for sourcmapepr

--- src/path_utils.mjs
+++ src/path_utils.mjs
@@ -4,9 +4,11 @@ export async function getGoPath()  {
     return new Promise((resolve, reject)  => {
         exec("id -un", (error, stdout, stderr) => {
             if (error) {
+                console.error(`getGoPath-err` , error)
                 reject(error)
             } else {
-                resolve(`/home/${stdout}/go/bin`)
+                console.error(`getGoPath-stdout` , stdout)
+                resolve(`/home/${stdout.trim()}/go/bin`)
             }
         });
     })
--- src/sourcemap.mjs
+++ src/sourcemap.mjs
@@ -2,32 +2,41 @@ import path from "node:path";
 import {getGoPath} from "./path_utils.mjs";
 import chalk from 'chalk';
 import {errorChalk} from "./text.mjs";
-import { existsSync} from "node:fs";
+import { existsSync, mkdirSync, readFileSync} from "node:fs";
 import {spawn} from "node:child_process";
+import { installSourceMapper } from "./pre_installation.mjs";
+const __dirname = import.meta.dirname;
+
 const validMapPattern = /\.js\.map$/i;
 export async function sourceMap(input, output) {
+    await installSourceMapper()
     const validLinksList = inputChecker(input)
     const outputPath = outputChecker(output);
-    await downloadSources(validLinksList)
+    await downloadSources(validLinksList , outputPath)
 }
-async function downloadSources(validLinksList) {
+async function downloadSources(validLinksList , outputPath) {
     const goPath = await getGoPath() 
     const sourcemapperPath = path.join(goPath, 'sourcemapper')
-    for(const link in validLinksList) {
-        const p = link.split(".js")[0]
-        fs.mkdirSync(p)
-        spawn(sourcemapperPath, [`-url ${link}` , `-output ${p}`]);
+    console.log('validLinksList :>> ', validLinksList);
+    for(const link of validLinksList) {
+        console.log('link :>> ', link);
+        const jsFile = link.split(".js")[0]
+        console.log('jsFile :>> ', jsFile);
+        const jsFileSavePath =path.join(outputPath , jsFile).split(":/")[1].replace(/\//g, "_")
+        console.log("jsFileSavePath" , jsFileSavePath)
+        mkdirSync(jsFileSavePath)
+        spawn(sourcemapperPath, [`-url ${link.trim()}` , `-output ${jsFileSavePath.trim()}`]);
     }
 }
 function inputChecker(input) {
     const validLinks = []
-    if(!fs.existsSync(input)){
+    if(!existsSync(input)){
         console.error(errorChalk('can not find input file'))
     }
     if(!['txt'].includes(input.split(".")[input.split(".").length - 1])){
         console.error(errorChalk('invalid file type txt is supported for now'))
     }
-    const content = fs.readFileSync(input , {encoding:'utf-8'}).toString()
+    const content = readFileSync(input , {encoding:'utf-8'}).toString()
     for (const link of content.split('\n')) {
         const trimmedLink = link.trim();
         if (!validMapPattern.test(trimmedLink)) {
@@ -42,9 +51,9 @@ function inputChecker(input) {
 function outputChecker(output) {
     let outputPath = path.join(__dirname , '..' , output)
     if(existsSync(outputPath)){
-        // outputPath = `${outputPath}${Math.floor((Math.random() * 1000 - 1) + 1)}`
+        outputPath = `${outputPath}${Math.floor((Math.random() * 1000 - 1) + 1)}`
     }
-    // fs.mkdirSync(outputPath)
+    mkdirSync(outputPath)
     return outputPath
 }
 
