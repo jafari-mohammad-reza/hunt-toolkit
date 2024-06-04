aa9ca427be004fa93929dca536e82fc248e7e5a7
Mohammadreza Jafari <mohammadrezajafari.dev@gmail.com>
2024-06-04 12:13:12 +0330

    fix: fix downloading all links into one output

--- src/sourcemap.mjs
+++ src/sourcemap.mjs
@@ -1,10 +1,8 @@
 import path from "node:path";
 import {getGoPath} from "./path_utils.mjs";
-import chalk from 'chalk';
-import {errorChalk} from "./text.mjs";
 import { existsSync, mkdirSync, readFileSync} from "node:fs";
-import {spawn} from "node:child_process";
 import { installSourceMapper } from "./pre_installation.mjs";
+import {execSync} from "child_process";
 const __dirname = import.meta.dirname;
 
 const validMapPattern = /\.js\.map$/i;
@@ -15,32 +13,25 @@ export async function sourceMap(input, output) {
     await downloadSources(validLinksList , outputPath)
 }
 async function downloadSources(validLinksList , outputPath) {
-    const goPath = await getGoPath() 
+    const goPath = await getGoPath()
     const sourcemapperPath = path.join(goPath, 'sourcemapper')
-    console.log('validLinksList :>> ', validLinksList);
     for(const link of validLinksList) {
-        console.log('link :>> ', link);
         const jsFile = link.split(".js")[0]
-        console.log('jsFile :>> ', jsFile);
-        const jsFileSavePath =path.join(outputPath , jsFile).split(":/")[1].replace(/\//g, "_")
-        console.log("jsFileSavePath" , jsFileSavePath)
+        const jsFileSavePath =path.join(outputPath , path.join(outputPath , jsFile).split(":/")[1].replace(/\//g, "_"))
         mkdirSync(jsFileSavePath)
-        spawn(sourcemapperPath, [`-url ${link.trim()}` , `-output ${jsFileSavePath.trim()}`]);
+        execSync(`${sourcemapperPath} -url ${link.trim()} -output ${jsFileSavePath.trim()}`);
     }
 }
 function inputChecker(input) {
     const validLinks = []
     if(!existsSync(input)){
-        console.error(errorChalk('can not find input file'))
     }
     if(!['txt'].includes(input.split(".")[input.split(".").length - 1])){
-        console.error(errorChalk('invalid file type txt is supported for now'))
     }
     const content = readFileSync(input , {encoding:'utf-8'}).toString()
     for (const link of content.split('\n')) {
         const trimmedLink = link.trim();
         if (!validMapPattern.test(trimmedLink)) {
-            console.log(chalk.red(`\n Invalid map source: ${trimmedLink} \n`));
         }else{
             validLinks.push(trimmedLink)
         }
